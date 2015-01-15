bdd0d870f3dfd8d68b89172c60e393e7dd5ca65c
Kang Hyojun <hyojun@admire.kr>
2015-01-15 16:07:22 +0900

    Implement basic ls cli

--- lspy/cli.py
+++ lspy/cli.py
@@ -4,6 +4,18 @@
 """
 import click
 
+from .dig import listing_informs
+from .represent import only_names
+
 @click.group()
 def cli():
     pass
+
+
+@cli.command()
+@click.argument('path', default='.')
+@click.option('--a', default=False, type=bool)
+def find(a, path):
+    infos = listing_informs(path)
+    for name in only_names(infos):
+        print(name)
--- lspy/represent.py
+++ lspy/represent.py
@@ -6,10 +6,20 @@ __all__ = 'only_names',
 
 
 def only_names(informs):
-    """Returns name of files or directorys
+    """Returns name of files or directories
 
     :param list informs: a list of :attr:`lspy.dig.Inform` .
-    :return: a list contains name of files or directorys
+    :return: a list contains name of files or directories
     :rtype: list
     """
     return [info.name for info in informs]
+
+
+def long_repr(informs):
+    """Return long informat of files or directories
+
+    :param list informs: a list of :attr:`lspy.dig.Inform` .
+    :return: a list contains long information of files or directories
+    :rtype: list
+    """
+    pass
